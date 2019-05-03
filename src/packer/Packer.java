c55b3325871f83f7b9b9e455a0ddcf9958f076d0
Rina Calangian <rina.calangian@yahoo.com>
2019-05-03 16:34:42 +1200

    Corrected codes on Manifest.java: has corrected some lines on method packProducts()

--- src/packer/Packer.java
+++ src/packer/Packer.java
@@ -11,15 +11,24 @@ import java.util.List;
 /**
  *
  * @author bunta
+ * @version 1.1
  */
 public class Packer {
-
+/**
+ * 
+ * @param c object of class Customer containing customer name and closest address
+ * @param d object of class Depot containing depot name and address
+ * @param m object of class Manifest containing list of products and quantities
+ * @return returns collection of created boxes of packed products (could be one or more)
+ */
     public static List<Box> packProducts(Customer c, Depot d, Manifest m) {
         List<Box> packedBoxes = new ArrayList<>();
         Box b = null;
         while (!m.isEmpty()) { // repeat until all items are packed
             if (b == null) {
-                b = new Box(d,c);
+                // commented-out code below as the arguments are wrongly placed
+                //b = new Box(d,c);
+                b = new Box(c, d);
             }
             Product prodToAdd = m.getHeaviestUnder(b.remainingCapacity());
             if (prodToAdd == null) {
@@ -33,7 +42,8 @@ public class Packer {
         }
         if (b != null) {
             packedBoxes.add(b);
-            packedBoxes.add(b);
+            // removed the second call to method
+            //packedBoxes.add(b);
         }
         return packedBoxes;
     }
