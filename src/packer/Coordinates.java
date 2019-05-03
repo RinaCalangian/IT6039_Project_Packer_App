b44409f6bfe56b95a9a9ac9d0735337d004042f6
Rina Calangian <rina.calangian@yahoo.com>
2019-05-03 12:49:56 +1200

    Debugged Coordinates.java: has changed code on companyDistanceTo() method

--- src/packer/Coordinates.java
+++ src/packer/Coordinates.java
@@ -8,12 +8,13 @@ package packer;
 /**
  *
  * @author I.M.Bad
+ * @version 1.1
  */
 public class Coordinates {
-    
+
     private final double x;
     private final double y;
-    
+
     public Coordinates(double x, double y) {
         this.x = x;
         this.y = y;
@@ -26,29 +27,47 @@ public class Coordinates {
     public double getY() {
         return y;
     }
-    
+
+    /**
+     *
+     * @param other: (x, y) coordinates of another address 
+     * @return dist: the computed distance 
+     */
     public double euclideanDistanceTo(Coordinates other) {
         double xDiff = other.getX() - this.getX();
         double yDiff = other.getY() - this.getY();
-        double dist = Math.pow((xDiff * xDiff + yDiff * yDiff),0.5);
+        double dist = Math.pow((xDiff * xDiff + yDiff * yDiff), 0.5);
         return dist;
     }
-    
+
+    /**
+     * 
+     * @param other: (x, y) coordinates of another address
+     * @return dist: the computed distance 
+     */
     public double manhattanDistanceTo(Coordinates other) {
         double xDiff = other.getX() - this.getX();
         double yDiff = other.getY() - this.getY();
         double dist = Math.abs(xDiff) + Math.abs(yDiff);
         return dist;
     }
-    
+
+    /**
+     * 
+     * @param other
+     * @return: the average of the Euclidean Distance to the customer 
+     * and the Manhattan Distance to the customer.
+     * The result is added with 1km as a company requirement
+     */
     public double companyDistanceTo(Coordinates other) {
         double xDiff1 = other.getX() - this.getX();
         double yDiff1 = other.getY() - this.getY();
-        double dist1 = Math.pow((xDiff1 * xDiff1 + yDiff1 * yDiff1),0.5);
+        double dist1 = Math.pow((xDiff1 * xDiff1 + yDiff1 * yDiff1), 0.5);
         double xDiff2 = other.getX() - this.getX();
         double yDiff2 = other.getY() - this.getY();
         double dist2 = Math.abs(xDiff2) + Math.abs(yDiff2);
-        return (dist1 + dist2)/2;
+        //return (dist1 + dist2)/2;
+        return ((dist1 + dist2) / 2) + 1;
     }
 
 }
